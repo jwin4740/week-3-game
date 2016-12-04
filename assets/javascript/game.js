942d43bd0bbcd074b305499fbb8a158cba2dfa02
James W <jwin4740@gmail.com>
2016-12-04 15:06:57 -0500

    lowes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,49 +12,49 @@ var newPa;
     randomWord = word[Math.floor(Math.random() * word.length)];
     var mainDiv = document.getElementById("board");
 	for (var i = 0, n = randomWord.length; i < n; i++)
-	{
-		var newPa = document.createElement("div");
-		newPa.innerHTML = randomWord[i];
-		mainDiv.appendChild(newPa);
-		newPa.setAttribute("id", "replace");
-	}
+		{
+			var newPa = document.createElement("div");
+			newPa.innerHTML = randomWord[i];
+			mainDiv.appendChild(newPa);
+			newPa.setAttribute("id", "replace");
+		}
 	console.log(randomWord);
 }
 
 
+setUp();
 
-function guesser (){
 document.onkeyup = function(event){ 
 	
 	userGuess = event.key;
 	var userGuessCap = userGuess.toUpperCase();
 	for (var i = 0, n = randomWord.length; i < n; i++)
 	{
-		if (userGuessCap != randomWord[i])
+		if (userGuessCap != randomWord[0])
 			{
 				document.getElementById("board").innerHTML = '<div id="replace">' + userGuessCap + '<div>';
 				var mainDiv = document.getElementById("board");
 				for (var j = 1, n = randomWord.length; j < n; j++)
 						{
 							var newPa = document.createElement("div");
-							newPa.innerHTML = randomWord[i];
+							newPa.innerHTML = randomWord[j];
 							mainDiv.appendChild(newPa);
 							newPa.setAttribute("id", "insert");
 						}	
 			}
 
-		else if (userGuessCap == randomWord[i])
-			{
-				document.getElementById("board").innerHTML = '<div id="insert">' + userGuessCap + '<div>';
-				var mainDiv = document.getElementById("board");
-				for (var k = 1, n = randomWord.length; i < n; k++)
-						{
-							var newPa = document.createElement("div");
-							newPa.innerHTML = randomWord[i];
-							mainDiv.appendChild(newPa);
-							newPa.setAttribute("id", "replace");
-						}	
-			}
+		// else if (userGuessCap == randomWord[0])
+		// 	{
+		// 		document.getElementById("board").innerHTML = '<div id="insert">' + userGuessCap + '<div>';
+		// 		var mainDiv = document.getElementById("board");
+		// 		for (var k = 1, n = randomWord.length; i < n; k++)
+		// 				{
+		// 					var newPa = document.createElement("div");
+		// 					newPa.innerHTML = randomWord[0];
+		// 					mainDiv.appendChild(newPa);
+		// 					newPa.setAttribute("id", "replace");
+		// 				}	
+		// 	}
 
 	}
 	// if 
@@ -69,13 +69,13 @@ document.onkeyup = function(event){
 	// 			}
 
 };
-}
 
-setUp();
 
-do {
 
-	guesser();
 
-}
-while ( win == false);
+// do {
+
+// 	guesser();
+
+// }
+// while ( win == false);
